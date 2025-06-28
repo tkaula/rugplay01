098db9f69a775a80a6e76d5d934002bdc01ab901
Face <69168154+face-hh@users.noreply.github.com>
2025-06-28 17:37:23 +0300

    fix tooltip ssr errors

--- website/src/lib/components/ui/tooltip/tooltip-trigger.svelte
+++ website/src/lib/components/ui/tooltip/tooltip-trigger.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import { Tooltip as TooltipPrimitive } from "bits-ui";
+	import { browser } from '$app/environment';
 
-	let { ref = $bindable(null), ...restProps }: TooltipPrimitive.TriggerProps = $props();
+	let { ref = $bindable(null), disabled = false, ...restProps }: TooltipPrimitive.TriggerProps = $props();
 </script>
 
-<TooltipPrimitive.Trigger bind:ref data-slot="tooltip-trigger" {...restProps} />
+{#if browser}
+	<TooltipPrimitive.Trigger bind:ref {disabled} data-slot="tooltip-trigger" {...restProps} />
+{:else}
+	<!-- SSR fallback -->
+	<div
+		bind:this={ref}
+		data-slot="tooltip-trigger"
+		{...Object.fromEntries(
+			Object.entries(restProps).filter(
+				([key]) => !key.startsWith('on:') // Remove Svelte event handlers
+			)
+		)}
+	></div>
+{/if}
