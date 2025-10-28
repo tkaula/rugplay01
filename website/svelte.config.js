899f5e648c7803f85762be663ba7f2b682c40ca6
tkaula <104594465+tkaula@users.noreply.github.com>
2025-10-28 14:21:53 +0200

    Update svelte.config.js

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,15 +1,15 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$components: 'src/components',
+			$lib: 'src/lib',
+			$routes: 'src/routes'
+		}
 	}
 };
 
